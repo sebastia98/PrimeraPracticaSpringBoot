e0c62b2c0220aead063b82e97ff2e8aacc6fa6db
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-04-27 23:29:41 +0200

    Feat: Dar de alta al alumno desde ServeiAlumnat

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,10 +1,13 @@
 package org.formacio.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServeiAlumnat {
 
+	@Autowired
+	private RepositoriAlumnes repositori;
 	
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
@@ -12,8 +15,14 @@ public class ServeiAlumnat {
 	 * Si el nom de l'alumne es null, no l'ha de donar d'alta
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
+	
 	public boolean matricula (int id, String alumne) {
-		return true;
+		if (alumne == null) {
+			return false;
+		} else {
+			repositori.altaAlumne(id, alumne);
+			return true;
+		}
 	}
 	
 }
