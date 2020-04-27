a5a6ca0898286f4925d672f3611e541a81f61cb0
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-04-27 23:35:10 +0200

    Feat: Crear PostContruct para Joan y Maria

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,5 +1,7 @@
 package org.formacio.component;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +17,12 @@ public class ServeiAlumnat {
 	 * Si el nom de l'alumne es null, no l'ha de donar d'alta
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
+	
+	@PostConstruct
+	public void init() {
+		repositori.altaAlumne(1, "Antonia");
+		repositori.altaAlumne(2, "Joan");
+	}
 	
 	public boolean matricula (int id, String alumne) {
 		if (alumne == null) {
